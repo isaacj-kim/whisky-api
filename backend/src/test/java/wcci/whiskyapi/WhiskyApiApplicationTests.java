a83b0df768c1ceeeb0d26974cdb134e909ab59e4
Isaacjk614 <isaacjk614@gmail.com>
2019-07-17 16:07:14 -0400

    initial

---
+++ backend/src/test/java/wcci/whiskyapi/WhiskyApiApplicationTests.java
@@ -0,0 +1,16 @@
+package wcci.whiskyapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhiskyApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
