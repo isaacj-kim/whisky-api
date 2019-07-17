c88d6e38f3fca9d900d47b0863e4c2e23c8d1f7d
Isaacjk614 <isaacjk614@gmail.com>
2019-07-17 16:03:39 -0400

    first init

---
+++ src/main/java/wcci/whiskyapi/WhiskyApiApplication.java
@@ -0,0 +1,13 @@
+package wcci.whiskyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskyApiApplication.class, args);
+	}
+
+}
