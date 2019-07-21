a7d745cf2361a15da8a1f2bec220b0095caca542
Isaacjk614 <isaacjk614@gmail.com>
2019-07-21 13:29:55 -0400

    new dev branch

---
+++ backend/src/main/java/wcci/whiskyapi/WhiskyLabelRepository.java
@@ -0,0 +1,11 @@
+    
+package wcci.whiskyapi;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WhiskyLabelRepository extends CrudRepository<WhiskyLabel, Long>{
+
+	WhiskyLabel findByLabelName(String LabelName);
+}
